deab67bdfdabf96d29c9514513972320d9daa06c
gdeleha <retrvce@gmail.com>
2016-08-29 17:29:20 +0600

    sharing, counters

--- p/landos/js/common.js
+++ p/landos/js/common.js
@@ -10,13 +10,32 @@ $(document).ready(function() {
 		addClassActive:true
 	});
 
-	setBar();
+	var users;
+
+	$.ajax({
+		type:     "GET",
+		url:      "https://crossorigin.me/http://178.62.220.240:3000/getStats",
+		dataType: "json",
+		success: function(data){
+			$("#f-user-count").text(data.userCount);
+			$("#f-jobs-count").text(data.jobCount);
+			$("#f-freelancer-count").text(data.freelancerCount);
+			$("#p-user-count").text(data.userCount);
+			users = data.userCount;
+			setBar();
+		}
+	});
+
+	function setBar() {
+		var val = Math.floor((users/1000)*100);
+		$('.progress .bar').each(function() {
+			$(this).attr('style', 'width:' + val + '%;background-position:0 ' + val + '%');
+		});
+		$("#user-percentage").text(val);
+	}
 
 });
 
-function setBar() {
-  $('.progress .bar').each(function() {
-  	var val = 44;
-    $(this).attr('style', 'width:' + val + '%;background-position:0 ' + val + '%');
-  });
-}
+$(window).load(function () {
+	  $(".owl-item").hide();
+});
