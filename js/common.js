031a6f27b8da76545a9ce8fde3b16811eb3c9830
Michail Eroxov <mixaileroxov@gmail.com>
2016-09-10 19:49:42 +0300

    animetion with fix parallax

--- js/common.js
+++ js/common.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 
 	var owl = $("#slider");
 
+
+	var users;
+	var needCount = 1000;
+	var data = {};
+
 	owl.owlCarousel({
 		navigation: false,
 		pagination: true,
@@ -12,6 +17,58 @@ $(document).ready(function() {
 		addClassActive:true,
 		afterAction: changeFeatureImg
 	});
+
+
+	setTimeout( function(){ $('.feature-img-bg').fadeIn()}, 400 );
+	setTimeout( function(){ $('.feature-img').fadeIn()}, 400 );
+	
+
+	$('#slider').animate({"opacity": "1", "left": 0 }, 400).css({"display":"block"});
+	$('.box-icon').addClass("active")
+
+	setTimeout( function(){ $('.stat-content').fadeIn() }, 500);
+	$('.github-icon').fadeIn();
+	$('.socials').fadeIn();
+
+	$.ajax({
+		type:     "GET",
+		url:      "https://crossorigin.me/http://178.62.220.240:3000/getStats",
+		dataType: "json",
+		success: function(data){
+
+			$('.section-stat').viewportChecker({
+			    callbackFunction: function(elem, action){
+			    	countTo( $("#f-user-count"), data.userCount );
+					countTo( $("#f-jobs-count"), data.jobCount );
+					countTo( $("#f-freelancer-count"), data.freelancerCount);
+			    },
+			    offset: "35%"
+			});
+
+
+			$('.targets').viewportChecker({
+			    callbackFunction: function(elem, action){
+
+			    	if( $("#f-user-count").text() == "" ){
+			    			countTo( $("#f-user-count"), data.userCount );
+			    	}
+			    	if(  $("#f-jobs-count").text() == "" ){
+			    			countTo(  $("#f-jobs-count"), data.jobCount );
+			    	}
+			    	if( $("#f-freelancer-count").text() == "" ){
+			    			countTo( $("#f-freelancer-count"), data.freelancerCount );
+			    	}
+					countTo( $("#p-user-count"), data.userCount );
+					users = data.userCount;
+					setBar( data.userCount, needCount );
+			    },
+			    offset: "20%"
+			});
+
+		}
+	});
+
+
 
 	function changeFeatureImg() {
 		switch (this.owl.currentItem) {
@@ -53,26 +110,39 @@ $(document).ready(function() {
 		};
 	};
 
-	var users;
 
-	$.ajax({
-		type:     "GET",
-		url:      "https://crossorigin.me/http://178.62.220.240:3000/getStats",
-		dataType: "json",
-		success: function(data){
-			$("#f-user-count").text(data.userCount);
-			$("#f-jobs-count").text(data.jobCount);
-			$("#f-freelancer-count").text(data.freelancerCount);
-			$("#p-user-count").text(data.userCount);
-			users = data.userCount;
-			setBar();
-		}
-	});
 
-	function setBar() {
-		var val = Math.floor((users/1000)*100);
-		$('.progress .bar').each(function() {
-			$(this).attr('style', 'width:' + val + '%;background-position:0 ' + val + '%');
+
+	function countTo( elem, to ){
+		var $this = elem;
+
+		jQuery({ Counter: 0 }).animate({ Counter: to || 0 }, {
+		    duration: 1000,
+		    easing: 'swing',
+		    step: function () {
+		      $this.text(Math.ceil(this.Counter));
+		    }
+		});
+	}
+
+	function setBar( to, needCount ) {
+
+		var val = Math.floor((users/needCount)*100);
+		var $this = $('.progress .bar');
+
+
+		$this.each(function() {
+
+			jQuery({ Counter: 0 }).animate({ Counter: val || 0 }, {
+			    duration: 1000,
+			    easing: 'swing',
+			    step: function () {
+
+			    	width = (Math.ceil(this.Counter));
+			      	$this.attr('style', 'width:' + width + '%;background-position:0 ' + width + '%');
+			    }
+			});
+
 		});
 		$("#user-percentage").text(val);
 	}
